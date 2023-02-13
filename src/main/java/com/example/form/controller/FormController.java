f47f0e0397711040fb913a1371770c66fafd9c15
lean456 <leandroblanco.456@gmail.com>
2023-02-13 23:37:19 +0100

    nueva validacion a formController, notnull

--- src/main/java/com/example/form/controller/FormController.java
+++ src/main/java/com/example/form/controller/FormController.java
@@ -1,11 +1,16 @@
 package com.example.form.controller;
 
+import com.example.form.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
 @Controller
 public class FormController {
     @GetMapping("/form")
@@ -16,11 +21,18 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(error -> {
+                errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
+
+            });
+            model.addAttribute("error",errores);
+            return "form";
+        }
         model.addAttribute("titulo", "Resultado form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 
